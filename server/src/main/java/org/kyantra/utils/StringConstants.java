9f77f21a6cc33a4e3fc40d42143b15b838e2fd33
Omkar Manjrekar <manjrekarom@gmail.com>
2018-01-02 10:34:13 +0530

    somewhat generic path for storing thing certificates

--- server/src/main/java/org/kyantra/utils/StringConstants.java
+++ server/src/main/java/org/kyantra/utils/StringConstants.java
@@ -1,6 +1,6 @@
 package org.kyantra.utils;
 
 public class StringConstants {
-    public static final String CERT_ROOT = "/home/kirito/Documents/aws-iot-certificates/";
+    public static final String CERT_ROOT = "/opt/aws-iot-certificates/";
     public static final String DEFAULT_POLICY = "mos-default";
 }
