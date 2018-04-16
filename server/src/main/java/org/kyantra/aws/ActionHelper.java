814abab3b4f92b3c4da2b55e88bbde7449f947ab
Omkar Manjrekar <manjrekarom@gmail.com>
2018-04-16 18:05:45 +0530

    refactores actions in seperate file

--- server/src/main/java/org/kyantra/aws/ActionHelper.java
+++ server/src/main/java/org/kyantra/aws/ActionHelper.java
@@ -1,5 +1,13 @@
 package org.kyantra.aws;
 
+import com.amazonaws.services.iot.model.Action;
+import com.amazonaws.services.iot.model.LambdaAction;
+import com.amazonaws.services.iot.model.MessageFormat;
+import com.amazonaws.services.iot.model.SnsAction;
+import org.kyantra.beans.SnsBean;
+import org.kyantra.dao.ConfigDAO;
+import sun.security.krb5.Config;
+
 // singleton
 public class ActionHelper {
 
@@ -16,6 +24,28 @@ public class ActionHelper {
             instance = new ActionHelper();
         }
         return instance;
+    }
+
+    public Action createSnsAction(SnsBean snsBean) {
+        // 1. create SnsAction
+        SnsAction snsAction = new SnsAction();
+        snsAction.withTargetArn(snsBean.getTopicARN())
+                .withMessageFormat(MessageFormat.RAW)
+                .withRoleArn(ConfigDAO.getInstance().get("iotRoleArn").getValue());
+
+        // 2. create Action
+        Action action = new Action().withSns(snsAction);
+        return action;
+    }
+
+    public Action createLambdaAction(String functionArn) {
+        // 1. create LambdaAction
+        LambdaAction lambdaAction = new LambdaAction();
+        lambdaAction.withFunctionArn(functionArn);
+
+        // 2. create Action
+        Action action = new Action().withLambda(lambdaAction);
+        return action;
     }
 
     public String[] getActionTypes() {
