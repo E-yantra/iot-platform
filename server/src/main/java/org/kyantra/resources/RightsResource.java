bae87510eab1564ba4698f0512ecb2d32b127695
Omkar Manjrekar <manjrekarom@gmail.com>
2018-05-31 18:12:15 +0530

    added authorization to Rights and PubSub

--- server/src/main/java/org/kyantra/resources/RightsResource.java
+++ server/src/main/java/org/kyantra/resources/RightsResource.java
@@ -4,20 +4,17 @@ import io.swagger.annotations.Api;
 import org.kyantra.beans.RightsBean;
 import org.kyantra.beans.RoleEnum;
 import org.kyantra.beans.UnitBean;
+import org.kyantra.beans.UserBean;
 import org.kyantra.dao.RightsDAO;
 import org.kyantra.dao.UnitDAO;
 import org.kyantra.dao.UserDAO;
+import org.kyantra.exceptionhandling.DataNotFoundException;
+import org.kyantra.exceptionhandling.ExceptionMessage;
+import org.kyantra.helper.AuthorizationHelper;
 import org.kyantra.interfaces.Secure;
 import org.kyantra.interfaces.Session;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 // TODO: 5/25/18 Add proper authorization on it
@@ -29,35 +26,69 @@ public class RightsResource extends BaseResource {
     @Path("get/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public String get(@PathParam("id") Integer id) {
-        RightsBean userBean = RightsDAO.getInstance().get(id);
-        return gson.toJson(userBean);
+        RightsBean rightsBean = RightsDAO.getInstance().get(id);
+
+        if (rightsBean == null)
+            throw new DataNotFoundException(ExceptionMessage.DATA_NOT_FOUND);
+
+        UnitBean targetUnit = rightsBean.getUnit();
+        UserBean userBean = (UserBean)getSecurityContext().getUserPrincipal();
+
+        if (!AuthorizationHelper.getInstance().checkAccess(userBean, targetUnit))
+            throw new ForbiddenException(ExceptionMessage.FORBIDDEN);
+
+        return gson.toJson(rightsBean);
     }
 
-
+    // TODO: 5/31/18 Test
     @POST
     @Path("update/{id}")
+    @Session
+    @Secure(roles = {RoleEnum.ALL,RoleEnum.WRITE}, subjectType = "right", subjectField = "parentId")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @Secure(roles = {RoleEnum.ALL,RoleEnum.WRITE}, subjectType = "right", subjectField = "parentId")
     public String update(@PathParam("id") Integer id,
                          @FormParam("role") RoleEnum role) {
         //TODO: "new UnitBean()" should be user id?
-        //Can a user have different rights on separate units, if he is owner of multiple units?
-        RightsDAO.getInstance().update(id,new UnitBean(),role);
-        RightsBean bean = RightsDAO.getInstance().get(id);
-        return gson.toJson(bean);
+        RightsBean rightsBean = RightsDAO.getInstance().get(id);
+
+        if (rightsBean == null)
+            throw new DataNotFoundException(ExceptionMessage.DATA_NOT_FOUND);
+
+        UnitBean targetUnit = rightsBean.getUnit();
+        UserBean userBean = (UserBean)getSecurityContext().getUserPrincipal();
+
+        if (!AuthorizationHelper.getInstance().checkAccess(userBean, targetUnit))
+            throw new ForbiddenException(ExceptionMessage.FORBIDDEN);
+
+        // remove UnitBean (second) parameter in update
+        // allow to change type of role of a user on a unit if the user doing this has right to the unit
+        rightsBean = RightsDAO.getInstance().update(id, role);
+        return gson.toJson(rightsBean);
     }
 
 
     @DELETE
     @Path("delete/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Session
     @Secure(roles = {RoleEnum.ALL,RoleEnum.WRITE}, subjectType = "right", subjectField = "parentId")
+    @Produces(MediaType.APPLICATION_JSON)
     public String delete(@PathParam("id") Integer id) {
+        RightsBean rightsBean = RightsDAO.getInstance().get(id);
+
+        if (rightsBean == null)
+            throw new DataNotFoundException(ExceptionMessage.DATA_NOT_FOUND);
+
+        UnitBean targetUnit = rightsBean.getUnit();
+        UserBean userBean = (UserBean)getSecurityContext().getUserPrincipal();
+
+        if (!AuthorizationHelper.getInstance().checkAccess(userBean, targetUnit))
+            throw new ForbiddenException(ExceptionMessage.FORBIDDEN);
+
         try {
             RightsDAO.getInstance().delete(id);
             return "{}";
-        }catch (Throwable t) {
+        } catch (Throwable t) {
             t.printStackTrace();
         }
         return "{}";
@@ -72,17 +103,28 @@ public class RightsResource extends BaseResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public String create(@FormParam("unitId") Integer unitId,
                          @FormParam("userId") Integer userId,
-                         @FormParam("Role") RoleEnum role){
+                         @FormParam("Role") RoleEnum role) {
+
+        UnitBean targetUnit = UnitDAO.getInstance().get(unitId);
+        UserBean userBean = (UserBean)getSecurityContext().getUserPrincipal();
+
+        if (targetUnit == null)
+            throw new DataNotFoundException(ExceptionMessage.DATA_NOT_FOUND);
+
+        if (!AuthorizationHelper.getInstance().checkAccess(userBean, targetUnit))
+            throw new ForbiddenException(ExceptionMessage.FORBIDDEN);
+
         try {
             RightsBean rights = new RightsBean();
             rights.setRole(role);
+            // different from above user
             rights.setUser(UserDAO.getInstance().get(userId));
-            rights.setUnit(UnitDAO.getInstance().get(unitId));
+            rights.setUnit(targetUnit);
             RightsBean right = RightsDAO.getInstance().add(rights);
 
             return gson.toJson(right);
 
-        }catch (Throwable t){
+        } catch (Throwable t) {
             t.printStackTrace();
         }
         return "{\"success\":false}";
